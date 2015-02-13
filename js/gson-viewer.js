faed02855c91f6b589605f3b6e2eaa553c988e69
pstarace <paolostarace@gmail.com>
2015-02-13 18:51:46 +0100

    added textbox

--- js/gson-viewer.js
+++ js/gson-viewer.js
@@ -1,12 +1,6 @@
 var map;
 var gson_layer;
 
-/*var req = new XMLHttpRequest();
-req.open('GET', document.location, false);
-req.send(null);
-var headers = req.getAllResponseHeaders().toLowerCase();
-//alert(headers);
-*/
 function getRequestParameter(name){
     if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
         return decodeURIComponent(name[1]);
@@ -18,24 +12,38 @@ function onLoad(){
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map); 
     //adding geojson
+    var uri = getRequestParameter("uri");
     gson_layer = L.geoJson().addTo(map);
-    if(getRequestParameter("uri"))
-        doAjax("GET", getRequestParameter("uri"), "", location.search.substring(1), null, true, 
-                function(result){
-                    try {
-                        var geojson = JSON.parse(result);
-                        gson_layer.addData(geojson);       
-                        map.fitBounds(gson_layer.getBounds());
-                    }catch(err){
-                        alert(err);
-                        mapReset();
-                    }
-                });
-    else 
-        mapReset();
+    render(uri, location.search.substring(1).replace("uri="+uri, ""));
 }
 
-function mapReset(){
+function go(){
+    var uri = document.getElementById("text-uri").value.split("?");
+    render(uri[0], uri[1]);
+}
+
+function render(uri, params){
+    //alert(uri+" "+params);
+    if(uri){
+        document.getElementById("text-uri").value = uri + "?" + params;
+        doAjax("GET", uri, "", params, null, true, 
+            function(result){
+                try {
+                    var geojson = JSON.parse(result);
+                    gson_layer.clearLayers();
+                    gson_layer.addData(geojson);       
+                    map.fitBounds(gson_layer.getBounds());
+                }catch(err){
+                    alert(err);
+                    reset();
+                }
+            });
+    }else{
+        reset();
+    }
+}
+
+function reset(){    
     map.setView([41.9100711,12.5359979], 3);
 }
 
@@ -54,7 +62,7 @@ function doAjax(method, host, service, query, data, asynch, callback){
                         var result = requestObj.responseText;
                         if(callback) callback(result);
                     }else{
-                        mapReset();
+                        reset();
                     }
 	        }catch(err){
 	        	alert(err);
